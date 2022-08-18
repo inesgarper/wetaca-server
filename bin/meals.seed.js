a73883215856cf56bae5e85c126db9bb0e1e80d0
Guille Ávila <guille9797@hotmail.com>
2022-08-18 17:11:22 +0200

    meals seed

---
+++ bin/meals.seed.js
@@ -0,0 +1,50 @@
+const meals = [
+    {
+        name: 'MERLUZA CON ALLADA GALLEGA, PANADERAS, CHAMPIÑONES Y CEBOLLITAS',
+        type: 'Platos Únicos',
+        ingredients: 'Merluza (32.1%) con allada (fumet blanco (agua, espina de pescado, cebolla, puerro, vino blanco, aceite de oliva, sal), Oloroso, vino blanco, aceite de oliva virgen extra, pan de picos, ajo, sal, pimentón dulce, goma de maíz y laurel), patatas panaderas (20.5%), cebollitas francesas (10.3%), champiñones (10.3%), aceite de oliva, ajo, orégano, pimienta negra y laurel.',
+        category: 'Pescado',
+        weight: '560',
+        price: 7.45,
+        images: {
+            finals: ['https://static.wetaca.com/products/872/detail/merluza-con-allada-gallega-panaderas-champinones-y-cebollitas_A.jpg',
+                'https://static.wetaca.com/products/872/detail/merluza-con-allada-gallega-panaderas-champinones-y-cebollitas_B.jpg'],
+            wip: ['https://static.wetaca.com/products/872/gallery/merluza-con-allada-gallega-panaderas-champinones-y-cebollitas_A.jpg',
+                'https://static.wetaca.com/products/872/gallery/merluza-con-allada-gallega-panaderas-champinones-y-cebollitas_B.jpg',
+                'https://static.wetaca.com/products/872/gallery/merluza-con-allada-gallega-panaderas-champinones-y-cebollitas_C.jpg'],
+        },
+        description: 'Para la base de la allada partimos de nuestro caldo de pescados hecho a partir de espinas, vino blanco, pimentón, ajo y aceite de oliva. Cocinamos la merluza a baja temperatura dentro de la salsa para que quede jugosa. Para rematar acompañamos con unas patatas panaderas y un mix de verduras.',
+        allergens: {
+            celery: false,
+            gluten: true,
+            crustaceans: false,
+            eggs: false,
+            fish: true,
+            lupin: false,
+            milk: false,
+            molluscs: false,
+            mustard: false,
+            peanuts: false,
+            sesame: false,
+            soybeans: false,
+            sulphurDioxide: false,
+            sulphites: false,
+        },
+        nutritionalValues: {
+            calories: 120.00,
+            totalFats: 6.90,
+            saturatedFat: 1.00,
+            carbs: 7.80,
+            protein: 5.30,
+            sugar: 2.00,
+            fiber: 1.40,
+            sodium: 0.50
+        },
+        lastWeekInMenu: new Date(),
+        popularity: {
+            averageRating: 3,
+            timesOrdered: 17
+        }
+
+    }
+]
