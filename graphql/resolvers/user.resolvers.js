bf9b1811ab8b4dd416c8331b66f49f5ab19af543
Guille Ávila <guille9797@hotmail.com>
2022-08-29 14:40:06 +0200

    auth

--- graphql/resolvers/user.resolvers.js
+++ graphql/resolvers/user.resolvers.js
@@ -1,29 +1,56 @@
 import User from './../../models/User.js'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
+
+const saltRounds = 10
+
 
 const userResolvers = {
 
     Query: {
-        hello: () => 'Hello'
+        hello: () => 'Hello',
+        getCurrentUser: (_, args, context) => context.currentUser
     },
 
     Mutation: {
         createUser: (_, args) => {
-            const user = new User({ ...args.user })
+
+            // checkear que no exista
+
+            const { password } = args.user
+
+            const salt = bcrypt.genSaltSync(saltRounds)
+            const hashedPassword = bcrypt.hashSync(password, salt)
+
+            const user = new User({ ...args.user, password: hashedPassword })
             return user.save()
+        },
+
+        login: async (_, args) => {
+
+            const { email, password } = args
+
+            const foundUser = await User.findOne({ email })
+
+            if (bcrypt.compareSync(password, foundUser.password)) {
+                const { name, lastName, email, _id } = foundUser
+                const payload = { name, lastName, email, _id }
+
+                console.log('el pauload', payload)
+
+                const authToken = jwt.sign(
+                    payload,
+                    process.env.TOKEN_SECRET,
+                    { algorithm: 'HS256', expiresIn: '6h' }
+                )
+
+                return {
+                    value: authToken
+                }
+            }
         }
 
     }
 }
 
 export default userResolvers
-
-// createUser: (root, args) => {
-//     const user = new User({ username: args.username })
-
-//     // podemos hacer un try catch o async await
-//     return user.save().catch(error => {
-//         throw new UserInputError(error.message, {
-//             invalidArgs: args
-//         })
-//     })
-// },
--- graphql/typeDefs/user.typeDefs.js
+++ graphql/typeDefs/user.typeDefs.js
@@ -6,21 +6,33 @@ const userTypeDefs = gql`
         name: String!
         lastName: String!
         email: String!
+        password: String!
         id: ID!
     }
    
     type Query{
         hello: String
+        getCurrentUser: User
+    }
+
+    type Token{
+        value: String!
     }
 
     input UserInput{
         name: String!
         lastName: String!
         email: String!
+        password: String!
     }
 
     type Mutation{
         createUser(user: UserInput): User
+
+        login(
+            email: String!
+            password: String!
+        ): Token
     }
 `
 
