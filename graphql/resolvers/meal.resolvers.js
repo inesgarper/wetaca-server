374b317449efc56829b4da79e1fea527883a187f
inesgarper <igperianez@gmail.com>
2022-08-30 16:07:19 +0200

    add meals crud

--- graphql/resolvers/meal.resolvers.js
+++ graphql/resolvers/meal.resolvers.js
@@ -2,15 +2,54 @@ import Meal from '../../models/Meal.js'
 
 const mealResolvers = {
 
+    MealType: {
+        UNIQUE: 'Platos Únicos',
+        LIGHT: 'Platos Ligeros',
+        FULL: 'Platos Completos',
+        VEGGIE: 'Veggie',
+        STARTER: 'Entrantes',
+        DESSERT: 'Postres'
+    },
+
+    MealCategory: {
+        MEAT: 'Carne',
+        CHICKEN: 'Pollo',
+        FISH: 'Pescado',
+        PASTA: 'Pasta',
+        RICE: 'Arroz',
+        GRATINATED: 'Gratinado',
+        LEGUME: 'Legumbres',
+        INTERNATIONAL: 'Internacional',
+        VEGGIE: 'Veggie',
+        STARTER: 'Entrante',
+        FULL: 'Completo',
+        LIGHT: 'Ligero',
+        DESSERT: 'Postre'
+    },
+
     Query: {
-        hello: () => 'Hello',
-        // getCurrentUser: (_, args, context) => context.currentUser
+        getAllMeals: async () => await Meal.find(),
+        getMealDetails: async (_, args) => await Meal.findById({ _id: args.mealID })
     },
 
     Mutation: {
         addMeal: (_, args) => {
             const meal = new Meal({ ...args.mealData })
             return meal.save()
+        },
+
+        updateMeal: async (_, args) => {
+            const { mealID, mealData } = args
+
+            const updatedMeal = await Meal.findByIdAndUpdate(mealID, { $set: mealData }, { new: true })
+            return updatedMeal
+        },
+
+        deleteMeal: async (_, args) => {
+            const { mealID } = args
+
+            const deletedMeal = await Meal.findByIdAndDelete(mealID)
+            return `The meal ${deletedMeal.name} was successfully deleted!`
         },
 
     }
